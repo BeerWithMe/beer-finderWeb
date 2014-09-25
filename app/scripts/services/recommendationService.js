ad8bfedf182f537fdf34e1ee47fbc976f528f8e4
Lauren <lpappone@gmail.com>
2014-09-25 09:01:08 -0700

    clean up styling, make recommendations template

--- app/scripts/services/recommendationService.js
+++ app/scripts/services/recommendationService.js
@@ -1,4 +1,4 @@
 angular.module('beerMeApp')
   .factory('recommend', function ($http){
-
+   
   }
--- server/routes.js
+++ server/routes.js
@@ -7,16 +7,7 @@ module.exports = function(app) {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  // app.get('/questionnaire', function (req, res) {
-  //   var testResponse = [{'id': 41220, 'name': 'Budweiser', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/1P45iR/upload_upBR4q-large.png'},
-  //   {'id': 58978, 'name': 'Racer 5 IPA', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/o1OELJ/upload_OutGJZ-large.png'},
-  //   {'id': 37259, 'name': 'Anchor Steam' , 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/Uiol9p/upload_drOw0u-large.png'},
-  //   {'id': 47942, 'name': 'Guinness Draught', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/StkEiv/upload_etArOb-large.png'},
-  //   {'id': 40135, 'name': 'Blue Moon Belgian White' , 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/dDXOEp/upload_SZEtGz-large.png'}];
-  //   res.send(testResponse);
-  // });
 
-  // when main.html sends a post request to /login
   app.post('/login', function(req, res){  //write login function in service file, controlled by main controller
     // grab the username and password
     var params = {
