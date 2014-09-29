45316f2b46a398acabb96f041ee80e2f8baefa91
Lauren <lpappone@gmail.com>
2014-09-29 14:45:03 -0700

    merge

--- app/scripts/services/questionnaireService.js
+++ app/scripts/services/questionnaireService.js
@@ -2,13 +2,14 @@ angular.module('beerMeApp')
   .factory('Questionnaire', function () {
     return { 
       initialBeers:  
-      [{'name': 'Budweiser', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/1P45iR/upload_upBR4q-large.png'},
-      {'name': 'Racer 5 IPA', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/o1OELJ/upload_OutGJZ-large.png'},
-      {'name': 'Anchor Steam' , 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/Uiol9p/upload_drOw0u-large.png'},
-      {'name': 'Guinness Draught', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/StkEiv/upload_etArOb-large.png'},
-      {'name': 'Blue Moon Belgian White' , 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/dDXOEp/upload_SZEtGz-large.png'},
-      {'name': 'Lagunitas - IPA', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/iLlMCb/upload_xp2OJo-large.png'},
-      {'name': 'Pliny the Elder', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/XAXGgF/upload_or9CTI-large.png'}],
+      [{'name': 'Anheuser-Busch InBev-Budweiser', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/1P45iR/upload_upBR4q-large.png'},
+      // {'name': 'Racer 5 IPA', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/o1OELJ/upload_OutGJZ-large.png'},
+      {'name': 'Anchor Brewing Company-Anchor Steam' , 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/Uiol9p/upload_drOw0u-large.png'},
+      {'name': 'Guinness-Guinness Draught', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/StkEiv/upload_etArOb-large.png'},
+      {'name': 'Blue Moon Brewing Company-Blue Moon Belgian White' , 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/dDXOEp/upload_SZEtGz-large.png'},
+      {'name': 'Lagunitas Brewing Company-IPA', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/iLlMCb/upload_xp2OJo-large.png'},
+      // {'name': 'Pliny the Elder', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/XAXGgF/upload_or9CTI-large.png'}
+      {'name': 'Magic Hat Brewing Company-#9', 'imgUrl': 'https://s3.amazonaws.com/brewerydbapi/beer/hB0QeO/upload_hLDTGW-large.png'}],
       
       counter: -1, //incremented to zero immediately on first invocation of changeBeer
       
@@ -18,7 +19,7 @@ angular.module('beerMeApp')
         this.counter++;
         if (this.counter >= this.initialBeers.length) {
           return {
-            message: "Cool!  Go check out your recommendations!"
+            message: "Cool! Go check out your recommendations!"
           } 
         } else {
           var newBeer = this.initialBeers[this.counter];
