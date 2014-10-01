dec836b4067a5d261a2b18bd3ec14ecc358e61df
Lauren <lpappone@gmail.com>
2014-10-01 14:55:45 -0700

    erase commentedout text

--- app/scripts/controllers/searchResults.js
+++ app/scripts/controllers/searchResults.js
@@ -3,35 +3,6 @@
 angular.module('beerMeApp')
 	.controller('searchResults',function($scope,searchResultsService,$stateParams){
 
-    // $scope.pour = function() {
-    //   jQuery(document).ready(function() {
-
-    //   console.log('pouring');
-    //   console.log('loading = ', $scope.loading)
-    //   jQuery('.pour') //Pour Me Another Drink, Bartender!
-    //     .delay(2000)
-    //     .animate({
-    //       height: '360px'
-    //       }, 1500)
-    //     .delay(1600)
-    //     .slideUp(500);
-      
-    //   jQuery('#liquid') // I Said Fill 'Er Up!
-    //     .delay(3400)
-    //     .animate({
-    //       height: '200px'
-    //     }, 2500);
-
-    //   jQuery('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
-    //     .delay(3400)
-    //     .animate({
-    //       bottom: '200px'
-    //       }, 2500);
-    //   });
-      
-    // };
-
-
     $scope.loading = true;
     searchResultsService.pour();
 
