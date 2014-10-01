26735ee505f6300fb453ce575543f914d7d12d3a
Lauren <lpappone@gmail.com>
2014-09-30 17:05:34 -0700

    added loading animation

--- app/scripts/services/searchResultsService.js
+++ app/scripts/services/searchResultsService.js
@@ -1,7 +1,9 @@
 angular.module('beerMeApp')
   .factory('searchResultsService',function($http){
   	var searchResultsService = {
-  		getResults: function(beername,callback){
+  		getResults: function(beername, callback){
+      // var loading = true;
+      // console.log('scope.loading= ', loading)
 			var data = JSON.stringify({beername: beername})
   			console.log('sending request to server for ...',data)
 			$http({
@@ -11,8 +13,12 @@ angular.module('beerMeApp')
 		    }).success(function(data,status){
 		    	//the data looks like this: [{abv:,ibu:,name:,etc...},{abv:,ibu:,name:,etc...}]
 		    	console.log('the data has been received',data)
+          // loading = false;
+          // console.log('scope.loading= ', loading)
 		    	callback(data)
 		    }).error(function(error,status){
+          // loading = false; 
+          // console.log('scope.loading= ', loading)
 		    	console.log('error: ',error)
 		    })
 		}
