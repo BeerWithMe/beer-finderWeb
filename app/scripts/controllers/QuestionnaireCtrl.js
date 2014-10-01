ac680e86f1fe36e034d803ca78879a55ff954dc3
Lauren <lpappone@gmail.com>
2014-10-01 10:45:34 -0700

    fix questionnaire bug

--- app/scripts/controllers/QuestionnaireCtrl.js
+++ app/scripts/controllers/QuestionnaireCtrl.js
@@ -3,10 +3,13 @@
 angular.module('beerMeApp')
   .controller('QuestionnaireCtrl', function ($scope, $routeParams, likeButton, Questionnaire, $location) {
 
+    Questionnaire.counter = 0;
+    console.log('counter = ', Questionnaire.counter)
     $scope.beername = Questionnaire.initialBeers[0].name;
     $scope.imgUrl = Questionnaire.initialBeers[0].imgUrl;
     $scope.beernameInDB = Questionnaire.initialBeers[0].beernameInDB;
     $scope.rate = 0;
+
 
     // loads the next beer in the predetermined survey list
     $scope.nextbeer = function(){
