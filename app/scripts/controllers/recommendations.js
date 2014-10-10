53c1279647387cf2a588d258a0232ab57b4d85a7
Lauren <lpappone@gmail.com>
2014-10-09 17:58:04 -0700

    clean up all controller code

--- app/scripts/controllers/recommendations.js
+++ app/scripts/controllers/recommendations.js
@@ -1,18 +1,18 @@
 'use strict';
 
+//this initiates the call to the server to generate recommendations, and receives the response array
+
 angular.module('beerMeApp')
   .controller('RecommendCtrl', function ($rootScope, $scope, $state, $stateParams, $window, recommendationsRequest) {
     
     $scope.recommendationsList = [];
 
     $scope.userName = localStorage.getItem('userName');
-    console.log('the current username is ',$scope.userName);
 
     recommendationsRequest.getRecommendation($scope.userName)
     	.success(function(data, status, headers, config){
     		 $scope.recommendationsList = data.beers;
-    	})
-    console.log("This is recommendationList: ", $scope.recommendationsList);
+    	});
 
     $scope.clicked = recommendationsRequest.clicked; 
   });
