92e0f76ed16b3ed23f3dede89facd13a7b45a390
Mike Lam <mikelam231@gmail.com>
2014-10-09 21:27:00 -0700

    New recommendation algorithm uses k-nn and returns locations

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -86,17 +86,31 @@ var generateSimilarityQuery = [
 var generateLikesQuery = 'MATCH (u:User),(b:Beer)\nWHERE u.username=({username}) AND b.name=({beername})\nMERGE (u)-[l:Likes {rating: ({rating})}]->(b)'
 var checkLikesQuery = "MATCH (u:User)-[l:Likes]->(b:Beer) WHERE u.username =({username}) AND b.name =({beername}) return l";
 var updateLikesQuery = "MATCH (u:User)-[l:Likes]->(b:Beer) WHERE u.username =({username}) AND b.name =({beername}) SET l.rating = ({rating})"
-var generateRecommendationQuery = ['MATCH (u1:User)-[r:Likes]->(b:Beer),',
-                                  '(u1)-[s:Similarity]-(u2:User {username:({username})})',
-                                  'WHERE NOT((u2)-[:Likes]->(b))',
-                                  'WITH b, s.similarity AS similarity,',
-                                  'r.rating AS rating', 
-                                  'ORDER BY b.name, similarity DESC',
-                                  'WITH b AS beer, COLLECT(rating)[0..3] AS ratings',
-                                  'WITH beer, REDUCE(s = 0, i IN ratings | s + i)*1.0 / LENGTH(ratings) AS reco ORDER BY reco DESC',
-                                  'RETURN beer AS Beer, reco AS Recommendation'].join('\n');
+var generateRecommendationQuery = [
+                                    "MATCH (u1:User)-[r:Likes]->(b:Beer)-[loc:longLat]->(location),",
+                                    "(u1)<-[s:Similarity]-(u2:User {username:({username})})",
+                                    "WHERE NOT ((u2)-[:Likes]->(b))",
+                                    "WITH b, r.rating AS rating,COLLECT(location) AS locations, s.similarity as similarity",
+                                    "WHERE similarity > 0.6",
+                                    "WITH b, locations, COLLECT(similarity) AS similarities, COLLECT(rating) AS ratings",
+                                    "WITH REDUCE(x = 0, i IN similarities | x+i)*1.0 / LENGTH(similarities) AS avgSimilarity,",
+                                    "locations,b,similarities, REDUCE(x = 0, i IN ratings | x+i)*1.0 / LENGTH(similarities) AS avgRating, ratings",
+                                    "ORDER BY avgRating DESC, LENGTH(ratings) DESC, avgSimilarity DESC",
+                                    "WHERE avgRating >2",
+                                    "RETURN b AS Beer, avgRating AS Recommendation, locations"
+                                  ].join('\n');
 
-
+//NEW RECOMMENDATION QUERY
+// MATCH (u1:User)-[r:Likes]->(b:Beer)-[loc:longLat]->(location),
+// (u1)<-[s:Similarity]-(u2:User {username:({username}))})
+// WHERE NOT ((u2)-[:Likes]->(b))
+// WITH b, r.rating AS rating,COLLECT(location) AS locations, s.similarity as similarity
+// WHERE similarity > 0.6
+// WITH b, locations, COLLECT(similarity) AS similarities, COLLECT(rating) AS ratings
+// WITH REDUCE(x = 0, i IN similarities | x+i)*1.0 / LENGTH(similarities) AS avgSimilarity,locations,b,similarities, REDUCE(x = 0, i IN ratings | x+i)*1.0 / LENGTH(similarities) AS avgRating, ratings
+// ORDER BY avgRating DESC, LENGTH(ratings) DESC, avgSimilarity DESC
+// WHERE avgRating >2
+// return b AS Beer, avgRating AS Recommendation
 
 db.getAllBeer = function(callback){
 	db.query(getAllBeerQuery, {}, function(err, allBeers){
