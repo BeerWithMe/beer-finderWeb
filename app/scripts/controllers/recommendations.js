741c5f5c6ab267a84aa4fbf6c99b65789d95ead3
Lauren <lpappone@gmail.com>
2014-10-09 17:48:42 -0700

    cleaning up code

--- app/scripts/controllers/recommendations.js
+++ app/scripts/controllers/recommendations.js
@@ -1,29 +1,7 @@
 'use strict';
 
-
-// fake data for testing
-// var recomList = [ {beer: {
-// 					name: "A",
-// 					ibu: 5,
-// 					abv: 3,
-// 				  	description: "A is originated from CA",
-// 				  	iconUrl: 'https://s3.amazonaws.com/brewerydbapi/beer/1P45iR/upload_upBR4q-large.png',
-// 				  	brewery: "A's brewery"
-// 				  },
-// 				  recommendation: 5},
-// 				  {beer: {
-// 				  	name: "B",
-// 				  	ibu: 4,
-// 				  	abv: 3,
-// 				  	description: "B is popular back in 1990",
-// 				  	iconUrl: 'https://s3.amazonaws.com/brewerydbapi/beer/o1OELJ/upload_OutGJZ-large.png',
-// 				  	brewery: "B's brewery"
-// 				  },
-// 				  recommendation: 4}
-// ];
-
 angular.module('beerMeApp')
-.controller('RecommendCtrl', function ($scope, $stateParams, $rootScope, $state, recommendationsRequest,$window) {
+  .controller('RecommendCtrl', function ($rootScope, $scope, $state, $stateParams, $window, recommendationsRequest) {
     
     $scope.recommendationsList = [];
 
@@ -37,9 +15,4 @@ angular.module('beerMeApp')
     console.log("This is recommendationList: ", $scope.recommendationsList);
 
     $scope.clicked = recommendationsRequest.clicked; 
-    // $scope.clicked = function(beername){
-    // 	$rootScope.beer = beername;
-    // 	console.log("This is $rootScope.beer", $rootScope.beer)
-    // 	$state.go('beer');
-    // }
-});
+  });
